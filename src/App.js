5131c1e02332f7f3a1645d4d810b6af676c4013c
nahid <sultannahid38@gmail.com>
2023-02-20 16:18:30 +0800

    added firebase config file and .env file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
 function App() {
+  console.log(process.env);
   return (
     <>
       <RouterProvider router={routes} />
-
     </>
   );
 }
