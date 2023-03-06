63172a9f77ef44a68270c5f5ae74ba4621e67294
nahid <sultannahid38@gmail.com>
2023-03-06 17:51:55 +0800

    added search option for find job

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -7,10 +7,12 @@ const Jobs = () => {
 
   return (
     <div className='pt-14 mx-7'>
-      <div className='bg-primary/10 p-5 rounded-2xl'>
+      <div className='bg-primary/10 p-5 rounded-2xl flex justify-between'>
         <h1 className='font-semibold text-xl'>Find Jobs</h1>
+        <input type="text" />
       </div>
-      <div className='grid grid-cols-2 gap-5 mt-5'>
+
+      <div className='grid grid-cols-2 gap-5 mt-5 py-5'>
         {
           data?.data?.map((jobData) => <JobCard jobData={jobData} />)
         }
